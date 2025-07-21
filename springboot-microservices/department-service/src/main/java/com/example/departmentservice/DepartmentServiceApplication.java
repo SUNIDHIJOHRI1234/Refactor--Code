deadf9b5b82453750039e2a2812f9a33df8f6dc9
SUNIDHIJOHRI1234 <119292749+SUNIDHIJOHRI1234@users.noreply.github.com>
2025-07-21 12:03:29 +0530

    Update DepartmentServiceApplication.java

--- springboot-microservices/department-service/src/main/java/com/example/departmentservice/DepartmentServiceApplication.java
+++ springboot-microservices/department-service/src/main/java/com/example/departmentservice/DepartmentServiceApplication.java
@@ -2,11 +2,11 @@ package com.example.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaClient;
+
 
 
 @SpringBootApplication
-@EnableEurekaClient
+
 
 public class DepartmentServiceApplication {
 
